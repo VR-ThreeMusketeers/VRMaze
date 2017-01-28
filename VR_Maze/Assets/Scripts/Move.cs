3d27a1c799e855d3d775cd843b6d5aebfc2798b8
karaluo09 <karaluo09@gmail.com>
2017-01-28 12:45:41 -0500

    Attempt angle change

--- VR_Maze/Assets/Scripts/Move.cs
+++ VR_Maze/Assets/Scripts/Move.cs
@@ -5,26 +5,37 @@ using Gvr;
 public class Move : MonoBehaviour {
 
 	public CharacterController player;
-	public int speed;
+	public Camera camera = Camera.main;
+	//private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
 		player = GetComponent <CharacterController> ();
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(GvrController.IsTouching){
+		//offset = camera.transform.rotation.eulerAngles - player.transform.rotation.eulerAngles;
+
+		if (GvrController.IsTouching) {
 			Vector2 movement = GvrController.TouchPos;
 			Vector3 move3 = new Vector3 ((float)(movement.x - .5), 0, -1 * (float)(movement.y - .5));
-			Quaternion quat = transform.rotation;
+			Vector3 newmove = camera.transform.TransformDirection (move3);
+			transform.rotation = Quaternion.LookRotation(newmove);
+			//Vector3 mod = move3.x * transform.forward + move3.z * transform.right;
+			//player.transform.Rotate (offset);
+
+			//player.transform.rotation = camera.transform.rotation;
+			player.Move (move3 * Time.deltaTime * 10);
+			/*Quaternion quat = transform.rotation;
 			Vector2 diference = new Vector2((quat.x - move3.x), (quat.z - move3.z));
 			float sign = (quat.z < move3.z)? -1.0f : 1.0f;
 			float angle = Vector2.Angle(Vector2.right, diference) * sign * Mathf.PI / 180;
 			Vector3 newvec = new Vector3 (Mathf.Cos (angle) * move3.x - Mathf.Sin (angle) * move3.z, 0, Mathf.Sin (angle) * move3.x + Mathf.Cos (angle) * move3.z);
-			player.Move (move3 * Time.deltaTime * 10);
+			player.Move (move3 * Time.deltaTime * 10);*/
 		}
-			
 	}
+
+
+
 }
